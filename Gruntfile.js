070af58dc2f8195d794dd0ca49f90c25a9a14692
Grizzlyslayer <65252136+Grizzlyslayer@users.noreply.github.com>
2020-11-20 07:03:09 -0500

    got tutorial working

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+      pkg: grunt.file.readJSON('package.json'),
+      connect: {
+        server: {
+          options: {
+            port: 8000,
+            hostname: 'localhost',
+            livereload: 35729
+          }
+        }
+      },
+      watch: {
+        options: {
+          livereload: 35729
+        },
+        files: ['**'],
+        tasks: []
+      }
+    });
+  
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask("server", ["connect", "watch"]);
+
+
+  };
